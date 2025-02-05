774d871d48eb052896176fb01c19f330f4a1ba3f
HadasYemini <hadassyemini@gmail.com>
2025-02-05 22:16:12 +0200

    fix

--- src/components/DowntimeReport.tsx
+++ src/components/DowntimeReport.tsx
@@ -1,10 +1,7 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ExportToExcel from "./ExportToExcel";
-import ExportToExcelAndSendToMail from "./ExportToExcelAndSendToMail";
 import ExportToPDF from "./ExportToPdf";
-import ExportToPDFAndSendToMail from "./ExportToPdfAndSendToMail";
 
 interface ClosedFault {
     machineName: string;
